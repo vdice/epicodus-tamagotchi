97e5e6ff19a316b62a01c42f36549f457a27a19a
Jing Wait & Vaughn Dice <student@epicodus.com>
2015-08-11 13:48:37 -0700

    initialize tamagotchi and pass spec

---
+++ spec/tamagotchi_spec.rb
@@ -0,0 +1,16 @@
+require("rspec")
+require("tamagotchi")
+
+describe(Tamagotchi) do
+  describe("#initialize") do
+    it("sets the name and life levels of a new Tamagotchi") do
+      my_pet = Tamagotchi.new("green dragon")
+      expect(my_pet.name()).to(eq("green dragon"))
+      expect(my_pet.food_level()).to(eq(10))
+      expect(my_pet.sleep_level()).to(eq(10))
+      expect(my_pet.activity_level()).to(eq(10))
+    end
+  end
+
+
+end
